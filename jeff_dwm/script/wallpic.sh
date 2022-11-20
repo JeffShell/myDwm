39d9c43b57bcc9579e5c47de90c964ff241cca59
jeff <875899952@qq.com>
2022-11-20 23:10:41 +0800

    设置壁纸脚本，可以为不同的屏幕设置不同指定路径的图片

--- jeff_dwm/script/wallpic.sh
+++ jeff_dwm/script/wallpic.sh
@@ -1,6 +1,26 @@
 #!/bin/sh
 # xrandr --output eDP-1-1 --mode 1920x1080 --rate 60.00
+si=0
+for file in $(ls ~/MyImage/standImg/)
+do
+  sarr[$si]=$file
+  ((si++))
+done
+
+ri=0
+for file in $(ls ~/MyImage/rowImg/)
+do
+  rarr[$ri]=$file
+  ((ri++))
+done
+sIndex=$((RANDOM %${#sarr[*]}))
+rIndex=$((RANDOM %${#rarr[*]}))
+
+standImg=~/MyImage/standImg/${sarr[sIndex]}
+rowImg=~/MyImage/rowImg/${rarr[rIndex]}
+
+
 while true; do
-  feh --bg-fill --randomiz ~/MyImage/*
+  feh --bg-fill $standImg $rowImg 
   sleep 1000 
 done
